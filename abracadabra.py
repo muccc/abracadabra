6db6095ad0144898a336602ed634c62409bc480b
Sebastian Steuer <iggy@tsar.(none)>
2010-12-26 20:33:49 +0100

    way better api!

--- abracadabra.py
+++ abracadabra.py
@@ -2,11 +2,12 @@ import socket
 import struct
 import datetime
 import time
+import types
 
-ACAB_PORT = 50023
-#ACAB_PORT = 43948
+#ACAB_PORT = 50023  #spooler
+ACAB_PORT = 43948
 #ACAB_PORT = 44203
-LOCALHOST = "127.0.0.1"
+ACAB_IP = "127.0.0.1"
 
 SCREEN_HEIGHT = 6
 SCREEN_WIDTH = 16
@@ -15,27 +16,131 @@ SCREEN_CHANNELS =3
 
 FRAME_DEFAULT_DURATION = 500
 
-class Color:
-    def __init__(self, color):
-        self.r, self.g, self.b = color
+class Mask:
+    def __init__(self, x=None, y=None):
+        self.fields = []
+        if not (x is None or y is None):
+            self.add(x,y)
+    
+    def all(self):
+        self = Mask().invert()
+        return self
     
     def invert(self):
-        self.r = 255-self.r
-        self.g = 255-self.g
-        self.b = 255-self.b
-        return (self.r,self.g,self.b)
+        new_fields = []
+        for x in range(SCREEN_WIDTH):
+            for y in range(SCREEN_HEIGHT):
+                if not (x,y) in self.fields:
+                    new_fields.append((x,y))
+        self.fields = new_fields
+        return self
+    
+    def add(self, x,y):
+        if (x > SCREEN_WIDTH-1 or
+            x < 0 or
+            y > SCREEN_HEIGHT-1 or
+            y < 0):
+            raise IndexError
+        
+        if not (x,y) in self.fields:
+            self.fields.append((x,y))
+        return self
+    
+    def add_mask(self, other):
+        for pos in other.fields:
+            self.add(pos)
+        return self
+    
+    def remove(self, x,y):
+        if (x > SCREEN_WIDTH-1 or
+            x < 0 or
+            y > SCREEN_HEIGHT-1 or
+            y < 0):
+            raise IndexError
+        
+        if (x,y) in self.fields:
+            self.fields.remove((x,y))
+        return self
+    
+    def remove_mask(self, other):
+        for pos in other.fields:
+            self.remove(pos)
+        return self
+        
+    def add_row(self, y):
+        self.add([(x,y) for x in range(SCREEN_WIDTH)])
+        return self
+
+    def add_col(self, x):
+        self.add([(x,y) for y in range(SCREEN_HEIGHT)])
+        return self
+    
+    def add_square(self, x1,y1, x2,y2):
+        self.add_area(range(x1,x2),range(y1,y2))
+        return self
+        
+    def add_area(self, lx, ly):
+        print lx
+        print ly
+        for x in lx:
+            for y in ly:
+                self.add(x,y)
+        print self.fields
+        return self
+    
+class Color:
+    
+    @staticmethod
+    def avg(list):
+        len = len(list)
+        tr=tg=tb=0
+
+        for c in list:
+            tr+=c.r
+            tg+=c.g
+            tb+=c.b
+        
+        return Color(tr/len,tg/len,tb/len)
+        
+    
+    def __init__(self, r=0,g=0,b=0, name=None):
+        if (type(r) != types.IntType or 
+            type(g) != types.IntType or
+            type(b) != types.IntType):
+            raise TypeError
+        
+        
+        if not name:
+            self.r = max(min(r,255),0)
+            self.g = max(min(g,255),0)
+            self.b = max(min(b,255),0)
+        else:
+            raise LookupError #TODO implement real lookup
+        
+    def invert(self):
+        return Color(255-self.r, 255-self.g, 255-self.b)
      
-    def get_data(self):
-        return "%02X%02X%02X"%(self.r,self.g,self.b)
+    def add(self,other):
+        return Color(self.r+other.r, self.g+other.g, self.b+other.b)
+    
+    def sub(self,other):
+        return Color(self.r-other.r, self.g-other.g, self.b-other.b)
+    
+    def avg_add(self,other):
+        return Color.avg((self,other))
+    
+    
+    def _to_data(self):
+        return "%s%s%s"%(chr(self.r),chr(self.g),chr(self.b))
 
 class Frame:
-    def __init__(self, duration = FRAME_DEFAULT_DURATION):
+    def __init__(self, duration = FRAME_DEFAULT_DURATION, color=Color()):
         #TODO check for bogus stuff here
         self.duration = duration
         self.content={}
         for x in range(SCREEN_WIDTH):
             for y in range(SCREEN_HEIGHT):
-                self.content[(x,y)]=(0,0,0)
+                self.content[(x,y)]=color
         
     def __unicode__(self):
         return "Duration: %s"%(self.duration)
@@ -43,24 +148,35 @@ class Frame:
     def __str__(self):
         return self.__unicode__()
     
-    def get_content_for_stream(self):
+    def _get_content_for_stream(self):
         data = ""
-        for x in range(SCREEN_WIDTH):
-            for y in range(SCREEN_HEIGHT):
-                r,g,b = self.content[(x,y)]
-                data+= "%s%s%s"%(chr(r),chr(g),chr(b))
+        for y in range(SCREEN_HEIGHT):
+            for x in range(SCREEN_WIDTH):
+            
+                #r,g,b = self.content[(x,y)]
+                data+= self.content[(x,y)]._to_data()
         return data
-        
     
-    def fill(self, color):
-        for pos in self.content:
+    def set(self, mask = Mask().all(), color=Color(255,255,255)):    
+        print mask.fields
+        for pos in mask.fields:
             self.content[pos]=color
+        return self
     
     def invert(self):
         for pos in self.content:
-            self.content[pos]=Color(self.content[pos]).invert()
+            self.content[pos]=self.content[pos].invert()
         return self
+    
+    def shift(self,dx=0,dy=0):
+        new_content = {}
+        for x in range(SCREEN_WIDTH):
+            for y in range(SCREEN_HEIGHT):
+                new_content[(x,y)]=self.content[((x-dx)%SCREEN_WIDTH,(y-dy)%SCREEN_HEIGHT)]
         
+        self.content= new_content
+        return self
+    
 class Stream:
     TITLE = "Untitled"
     AUTHOR = "Anonymous"
@@ -73,15 +189,17 @@ class Stream:
     def next_frame(self, last_frame):
         pass 
     
-    def run(self):
+    def run(self,):
         connection = Connection()
         #hello = connection.receive_zero_terminated()
         #print hello
-        if connection.request(self.TITLE,self.AUTHOR):
+        if 1:# connection.request(self.TITLE,self.AUTHOR):
             print "Starting Stream"
             frame = self.init_frame()
+            print frame
             try:
                 while frame: 
+                    print "bla"
                     connection.send_frame(frame)
                     frame = self.next_frame(frame)
             except KeyboardInterrupt:
@@ -90,7 +208,7 @@ class Stream:
         connection.close()
     
 class Connection():
-    def __init__(self, ip=LOCALHOST, port=ACAB_PORT):
+    def __init__(self, ip=ACAB_IP, port=ACAB_PORT):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((ip, port))
         self.rx_buffer = ''
@@ -121,7 +239,7 @@ class Connection():
         self.socket.send(struct.pack('!III', HEADER | OP_DURATION, 8+4, duration))
         d = struct.pack('!II', HEADER | OP_SET_SCREEN | MASK_ACK,
                             8 + SCREEN_HEIGHT * SCREEN_WIDTH * SCREEN_DEPTH / 8 * SCREEN_CHANNELS)
-        d+= frame.get_content_for_stream()
+        d+= frame._get_content_for_stream()
         
         #delta = (self.previous_sent+datetime.timedelta(milliseconds=self.previous_duration))-datetime.datetime.now()
         #if delta >= datetime.timedelta(microseconds = 10):
--- example-invert.py
+++ example-invert.py
@@ -1,13 +1,36 @@
-import abracadabra
+from abracadabra import Frame, Color,Stream, Mask
 
-class InvertStream(abracadabra.Stream):
+class InvertStream(Stream):
     def init_frame(self):
-        frame = abracadabra.Frame(1000)
-        frame.fill((255,0,0))
+        frame = Frame(200)
+    
+        frame.set(color = Color(255,0,0))
+        frame.set(Mask(0,0).add_area((0,15), (0,3)), Color(0,255,0))
+        return frame
+#        red = Color(255,0,0)
+#        red = (255,0,0)
+#        red.invert()
+#        mymask = Mask().add_column(1).invert()
+#        
+#        frame = abracadabra.Frame(1000)
+#        frame.fill(Color(255,0,0))
+#        
+#        frame.set(Pos(10,3),Color(0,255,0))
+#        frame.set(Color(0,255,0))
+#        frame.pos(10,3)=color(255,0,0)
+#        frame.pos(10,3).color(255,0,0)
+#        
+#        frame[(10,3)]=Color("red")
+#        frame.fill(Color("red"), mask=Mask.square((10,2),(15,4)))
+#        
+#        frame.mask(square, ).fill
+
+        Mask().add_square([0,0],[4,4])
         return frame
     
     def next_frame(self, last_frame):
-        return last_frame.invert()
+        print last_frame
+        return last_frame.shift(dx=1,dy=1)
     
 
 if __name__ == "__main__":
